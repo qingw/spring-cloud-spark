a9bfa776c978357dab8785dd3d73f38c4d7439d4
pinker <xd03122049@126.com>
2017-09-27 16:52:40 +0800

    update

--- config-server/src/main/java/org/sselab/ConfigServerApplication.java
+++ config-server/src/main/java/org/sselab/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package org.sselab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -10,7 +11,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
-    public static void main(String[] args) {
-        new SpringApplication(ConfigServerApplication.class).run(args);
-    }
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(ConfigServerApplication.class).web(true).run(args);
+  }
 }
